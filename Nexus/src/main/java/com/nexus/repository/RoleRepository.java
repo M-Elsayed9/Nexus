7e063839f79ef2f69be0a21f859c901379c83575
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-03-06 23:07:26 -0500

    add user and role repositories

--- Nexus/src/main/java/com/nexus/repository/RoleRepository.java
+++ Nexus/src/main/java/com/nexus/repository/RoleRepository.java
@@ -1,2 +1,10 @@
-package com.nexus.repository;public interface RoleRepository {
+package com.nexus.repository;
+
+import com.nexus.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
--- Nexus/src/main/java/com/nexus/repository/UserRepository.java
+++ Nexus/src/main/java/com/nexus/repository/UserRepository.java
@@ -1,2 +1,14 @@
-package com.nexus.repository;public interface UserRepository {
+package com.nexus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nexus.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    User findByUserName(String userName);
 }
