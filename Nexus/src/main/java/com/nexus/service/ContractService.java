7fa36023232511938391cabcfaf008a815f03b1f
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-03-26 21:01:45 -0400

    refactor services

--- Nexus/src/main/java/com/nexus/service/ContractService.java
+++ Nexus/src/main/java/com/nexus/service/ContractService.java
@@ -14,30 +14,31 @@ public class ContractService {
         this.contractRepository = contractRepository;
     }
 
+    //use dto, ensure the set of the contracts in the user entity is added
     @Transactional
     public void addContract(Contract contract) {
         contractRepository.save(contract);
     }
 
+    //use dto
     @Transactional(readOnly = true)
     public Contract getContractById(Long id) {
         return contractRepository.findById(id).orElse(null);
     }
 
+    //use dto
     @Transactional(readOnly = true)
     public List<Contract> getAllContracts() {
         return contractRepository.findAll();
     }
 
+    //use dto
     @Transactional
     public Contract updateContract(Contract contract) {
         return contractRepository.save(contract);
     }
 
+    //use dto, ensure the contract is removed from contracts in the user entity is removed
     @Transactional
     public void deleteContract(Long id) { contractRepository.deleteById(id); }
-
-    @Transactional
-    public void deleteAllContracts(Long id) { contractRepository.deleteAll(); }
-
 }
--- Nexus/src/main/java/com/nexus/service/OfficeDetailsService.java
+++ Nexus/src/main/java/com/nexus/service/OfficeDetailsService.java
@@ -1,9 +1,11 @@
 package com.nexus.service;
 
+import com.nexus.dto.OfficeDetailsDTO;
 import com.nexus.entity.OfficeDetails;
 import com.nexus.repository.OfficeDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,22 +19,20 @@ public class OfficeDetailsService {
         this.officeDetailsRepository = officeDetailsRepository;
     }
 
-    public List<OfficeDetails> getAllOfficeDetails() {
-        return officeDetailsRepository.findAll();
-    }
-
+    //use dto
+    @Transactional
     public OfficeDetails saveOfficeDetails(OfficeDetails officeDetails) {
         return officeDetailsRepository.save(officeDetails);
     }
 
+    //use dto
+    @Transactional(readOnly = true)
     public OfficeDetails getOfficeDetailsById(Long id) {
         return officeDetailsRepository.findById(id).orElse(null);
     }
 
-    public void deleteOfficeDetails(Long id) {
-        officeDetailsRepository.deleteById(id);
-    }
-
+    //use dto
+    @Transactional
     public OfficeDetails updateOfficeDetails(OfficeDetails officeDetails) {
         return officeDetailsRepository.save(officeDetails);
     }
